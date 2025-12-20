f135247fee092f51d86a542f016b40f8aa69c7d3
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2025-12-21 05:43:16 +0900

    feat: 상세페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home/Home';
-import Fridge from './pages/Fridge/Fridge';
-import Receipt from './pages/Receipt/Receipt';
-import Search from './pages/Search/Search';
-import MyPage from './pages/MyPage/MyPage';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import Fridge from "./pages/Fridge/Fridge";
+import Receipt from "./pages/Receipt/Receipt";
+import Search from "./pages/Search/Search";
+import MyPage from "./pages/MyPage/MyPage";
+import RecipeDetail from "./pages/RecipeDetail/RecipeDetail";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="fridge" element={<Fridge />} />
         <Route path="receipt" element={<Receipt />} />
         <Route path="search" element={<Search />} />
+        <Route path="recipe/:recipeId" element={<RecipeDetail />} />
         <Route path="mypage" element={<MyPage />} />
       </Route>
     </Routes>
