8b5fc531f7a4b31a08eb86ca8df0f097ec69adef
taegeon2 <154239852+taegeon2@users.noreply.github.com>
2025-12-28 17:02:31 +0900

    #4 TanStackQuery 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 
+// React Query Client 생성
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
 );
